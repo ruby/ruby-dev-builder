714cdf430c5bf8a3b42ac1e4a1a11cecb1408443
MSP-Greg <Greg.mpls@gmail.com>
2025-06-26 12:49:34 -0500

    cli_test.rb - add erb, test-unit & typeprof, fix bundler

--- cli_test.rb
+++ cli_test.rb
@@ -8,11 +8,18 @@ module CLITest
 
     def chk_cli(cmd, regex)
       cmd_str = cmd[/\A[^ ]+/].ljust(10)
-      if File.exist? "#{BIN_DIR}/#{cmd_str}".strip
+      cmd_path = "#{BIN_DIR}/#{cmd_str}".strip
+      if File.exist? cmd_path.strip
+        if RUBY_PLATFORM[/mingw|mswin/]
+          unless File.exist?("#{cmd_path}.bat") || File.exist?("#{cmd_path}.cmd")
+            return "#{cmd_str}❌   missing windows binstub"
+          end
+        end
         require 'open3'
         ret = ''.dup
         Open3.popen3(cmd) {|stdin, stdout, stderr, wait_thr|
           ret = stdout.read.strip
+          ret = stderr.read.strip if ret.empty?
         }
         if ret[regex]
           "#{cmd_str}✅   #{$1}"
@@ -26,14 +33,15 @@ module CLITest
       end
     rescue => e
       @error += 1
-      "#{cmd_str}❌   #{e.class}"
+      "#{cmd_str}❌   #{e.class} #{e.message}"
     end
 
     def run
       re_version = '(\d{1,2}\.\d{1,2}\.\d{1,2}(\.[a-z0-9.]+)?)'
       @error = 0
       puts "\n#{DASH * 5} CLI Test #{DASH * 17}"
-      puts chk_cli("bundle -v",      /\ABundler version #{re_version}/)
+      puts chk_cli("bundle -v",      /\A(?:Bundler version )?#{re_version}/)
+      puts chk_cli("erb --version",  /\A#{re_version}/)
       puts chk_cli("gem --version",  /\A#{re_version}/)
       puts chk_cli("irb --version",  /\Airb +#{re_version}/)
       puts chk_cli("racc --version", /\Aracc version #{re_version}/)
@@ -41,6 +49,8 @@ module CLITest
       puts chk_cli("rbs -v" , /\Arbs #{re_version}/)
       puts chk_cli("rdbg -v", /\Ardbg #{re_version}/)
       puts chk_cli("rdoc -v", /\A#{re_version}/)
+      puts chk_cli("test-unit --version", /\A(?:test-unit )?#{re_version}/)
+      puts chk_cli("typeprof --version", /#{re_version}\z/)
       puts ''
 
       cli_desc =  %x[ruby -v].strip
